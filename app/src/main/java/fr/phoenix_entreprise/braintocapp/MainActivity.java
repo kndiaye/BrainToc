05a1ab380ff21e0e5fc28d5c75114ab38a893d48
valentin <v.marcon@outlook.fr>
2015-12-14 10:38:33 +0100

    testGitOnAndroid

--- app/src/main/java/fr/phoenix_entreprise/braintocapp/MainActivity.java
+++ app/src/main/java/fr/phoenix_entreprise/braintocapp/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+            //
         }
 
         return super.onOptionsItemSelected(item);
