ab6e96b62ffa183e525159f62cf40cd083a84053
Luc-Menudier <luc1993@live.fr>
2016-01-11 17:49:44 +0100

    Implementation of the link with the database PHP scripts host on the local server Need to develop the register function of the RegisterActivity

--- app/src/main/java/fr/phoenix_entreprise/braintocapp/RegisterActivity.java
+++ app/src/main/java/fr/phoenix_entreprise/braintocapp/RegisterActivity.java
@@ -5,9 +5,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by valentin on 14/12/2015.
@@ -21,4 +31,52 @@ public class RegisterActivity extends AppCompatActivity{
         setContentView(R.layout.activity_register);
     }
 
+    public void register(View view) {
+        /*
+        String result = "";
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("year","1980"));
+
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://example.com/getAllPeopleBornAfter.php");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+        }catch(Exception e){
+            Log.e("log_tag", "Error in http connection " + e.toString());
+        }
+
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+
+            result=sb.toString();
+        }catch(Exception e){
+            Log.e("log_tag", "Error converting result "+e.toString());
+        }
+
+        try{
+            JSONArray jArray = new JSONArray(result);
+            for(int i=0;i<jArray.length();i++){
+                JSONObject json_data = jArray.getJSONObject(i);
+                Log.i("log_tag","id: "+json_data.getInt("id")+
+                                ", name: "+json_data.getString("name")+
+                                ", sex: "+json_data.getInt("sex")+
+                                ", birthyear: "+json_data.getInt("birthyear")
+                );
+            }
+        }catch(JSONException e){
+            Log.e("log_tag", "Error parsing data "+e.toString());
+        }
+        */
+        Intent intent = new Intent(this, ConnectionActivity.class);
+        startActivity(intent);
+    }
 }
