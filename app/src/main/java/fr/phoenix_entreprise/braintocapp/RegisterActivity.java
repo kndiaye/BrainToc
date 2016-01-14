53fec646e289c0ac076605a47836a4a5167d3f8a
Luc-Menudier <luc1993@live.fr>
2016-01-14 18:03:16 +0100

    Registration OK Need to validate the data before insert them in db in the register function

--- app/src/main/java/fr/phoenix_entreprise/braintocapp/RegisterActivity.java
+++ app/src/main/java/fr/phoenix_entreprise/braintocapp/RegisterActivity.java
@@ -13,6 +13,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -25,6 +26,8 @@ import java.util.Map;
  * Created by valentin on 14/12/2015.
  */
 public class RegisterActivity extends AppCompatActivity{
+
+    private static final String REGISTER_URL = "http://192.168.24.41/HumanICM/inscription.php";
 
     public static final String KEY_PSEUDO = "pseudo";
     public static final String KEY_NAISSANCE = "naissance";
@@ -58,47 +61,46 @@ public class RegisterActivity extends AppCompatActivity{
         final String naissance = editTextNaissance.getText().toString().trim();
         final String login = editTextLogin.getText().toString().trim();
         final String password = editTextPassword.getText().toString().trim();
-        int sex = groupSex.getCheckedRadioButtonId();
-        if(sex == R.id.radioButtonHomme){ sex = 1; }
-        if(sex == R.id.radioButtonFemme){ sex = 0; }
+        int idSex = groupSex.getCheckedRadioButtonId();
+        if(idSex == R.id.radioButtonHomme){ idSex = 1; }
+        if(idSex == R.id.radioButtonFemme){ idSex = 0; }
+        final int sex = idSex;
         Log.i(KEY_PSEUDO, pseudo);
         Log.i(KEY_NAISSANCE, naissance);
         Log.i(KEY_LOGIN, login);
         Log.i(KEY_PASSWORD, password);
         Log.i(KEY_SEXE, String.valueOf(sex));
 
-        String url = "http://192.168.24.41/HumanICM/inscription.php";
-        Map<String, String> params = new HashMap<String, String>();
-        params.put(KEY_PSEUDO, pseudo);
-        params.put(KEY_NAISSANCE, naissance);
-        params.put(KEY_LOGIN, login);
-        params.put(KEY_PASSWORD, password);
-        params.put(KEY_SEXE, String.valueOf(sex));
-
-        CustomRequest jsObjRequest = new CustomRequest(Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
-
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response: ", response);
+                        Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Response: ", error.toString());
+                        Toast.makeText(RegisterActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
             @Override
-            public void onResponse(JSONObject response) {
-                //try {
-                    Log.d("Response: ", response.toString());
-                    Toast.makeText(RegisterActivity.this, response.toString(), Toast.LENGTH_LONG).show();
-                //} catch (JSONException e) {
-                //    // TODO Auto-generated catch block
-                //    e.printStackTrace();
-                //}
-
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put(KEY_PSEUDO, pseudo);
+                params.put(KEY_NAISSANCE, naissance);
+                params.put(KEY_LOGIN, login);
+                params.put(KEY_PASSWORD, password);
+                params.put(KEY_SEXE, String.valueOf(sex));
+                return params;
             }
-        }, new Response.ErrorListener() {
 
-            @Override
-            public void onErrorResponse(VolleyError response) {
-                Log.d("Response: ", response.toString());
-                Toast.makeText(RegisterActivity.this, response.toString(), Toast.LENGTH_LONG).show();
-            }
-        });
+        };
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsObjRequest);
+        requestQueue.add(stringRequest);
         Intent intent = new Intent(this, ConnectionActivity.class);
         startActivity(intent);
     }
